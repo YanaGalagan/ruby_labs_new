e3c21993c40ecb8e03e42984a6230e88b096bb4f
Яна Александровна Галаган <s0160132@edu.kubsu.ru>
2023-04-07 20:46:52 +0300

    тест селекта из main

--- lab2/main.rb
+++ lab2/main.rb
@@ -2,27 +2,14 @@
 require_relative 'student/student'
 require_relative 'student/student_short'
 require_relative 'all_data/data_table'
+require 'yaml'
+require 'sqlite3'
 
 student1 = Student.new(first_name:'Иван', middle_name:'Иванович' ,surname:'Иванов',id:1,phone_number:'+79285698741',
                        mail:'ivan555@yandex.ru', git:'github.com/vanSuper', telegram:'@Vanusha')
 
 student2 = Student.new(first_name:'Илья',middle_name:'Александрович',surname:'Петров', id:2, phone_number:'+79285698741',
                        mail:'ilisha@mail.ru')
-
-
-
-# student2.set_contacts(telegram: '@petrov2002')
-
-
-# puts student1.to_s
-# puts student2.get_info
-
-# student3 = StudentShort.new(student2)
-# puts student3.to_s
-
-#puts Student.pars_str('{"first_name":"Илья","middle_name":"Александрович",
-#	"surname":"Петров", "id": 2, "phone_number":"+79285698741",
-#	"mail":"ilisha@mail.ru"}')
 
 
 def read_from_txt(adress)
@@ -59,15 +46,7 @@ def write_to_txt(adress, arr_student)
   end
 end
 
-#student_st= read_from_txt('test_read.txt')
-#puts student_st
-#student5 = StudentShort.new(student_st[0])
-#puts student5
-
-#student_list = [student1, student2]
-
-#write_to_txt('output.txt', student_list)# frozen_string_literal: true
-test = [[1,'Ура1'],[2,'Ура2']]
-test_table=DataTable.new(test)
-puts test_table
-puts test_table.get_elem(0,1)
+db = SQLite3::Database.open './university.sql'
+sel = db.prepare "Select * from students"
+result = sel.execute
+result.each {|th| puts th.join "\s"}
