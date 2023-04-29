facfa9dbf00af4f3df763ad6ef24ad103ee98f2c
Яна Александровна Галаган <s0160132@edu.kubsu.ru>
2023-04-29 18:09:48 +0300

    изменение окна

--- lab2/database/logic.rb
+++ lab2/database/logic.rb
@@ -1,15 +1,35 @@
 
 require 'glimmer-dsl-libui'
 require_relative 'controller'
+require_relative '../all_data/data_table'
 class LogicFromWindow
   include Glimmer
 
+  STUDENTS_PER_PAGE = 15
   def initialize
     @controller = StudentListController.new(self)
+    @current_page = 1
+    @total_count = 0
+  end
+
+  def on_create
+    @controller.on_view_created
+    @controller.refresh_data(@current_page, STUDENTS_PER_PAGE)
+  end
+
+  def on_datalist_changed(new_table)
+    arr = new_table.to_my_array
+    arr.map { |row| row[3] = [row[3][:phone] || row[3][:telegram] || row[3][:email]] }
+    @table.model_array = arr
+  end
+
+  def update_student_count(new_cnt)
+    @total_count = new_cnt
+    @page_label.text = "#{@current_page} / #{(@total_count / STUDENTS_PER_PAGE.to_f).ceil}"
   end
 
   def create
-    horizontal_box {
+    root_container = horizontal_box {
 
       # 1 область
       vertical_box {
@@ -54,36 +74,40 @@ class LogicFromWindow
 
       #2 область
       vertical_box {
-
         stretchy true
-        @table = table {
-          text_column('ФИО') {
-            on_clicked do
-              sort_by_column(0)
-            end
+        @table = refined_table(
+          table_editable: false,
+          table_columns: {
+            '#' => :text,
+            'Фамилия И. О' => :text,
+            'Гит' => :text,
+            'Контакт' => :text
           }
-          text_column('Гит') {
-            on_clicked do
-              sort_by_column(2)
-            end
-          }
-          text_column('Контакт') {
-            on_clicked do
-              sort_by_column(1)
-            end
-          }
+        )
 
-          editable false
+        @pages = horizontal_box {
+          stretchy false
 
-          cell_rows [['Иванов И.И. ', '@vinya', 'ivan@mail.ru'],
-                     ['Петров П.П.', '@petr', '+79384568921'],
-                     ['Сидоров С.С.', '@sidorov567', 'sidorsidorov@mail.ru']]
+          button("<") {
+            stretchy true
+
+            on_clicked do
+              @current_page = [@current_page - 1, 1].max
+              @controller.refresh_data(@current_page, STUDENTS_PER_PAGE)
+            end
+
+          }
+          @page_label = label("...") { stretchy false }
+          button(">") {
+            stretchy true
+
+            on_clicked do
+              @current_page = [@current_page + 1, (@total_count / STUDENTS_PER_PAGE.to_f).ceil].min
+              @controller.refresh_data(@current_page, STUDENTS_PER_PAGE)
+            end
+          }
         }
       }
-
-
-
-
       # 3 область
       vertical_box {
         stretchy true
@@ -91,9 +115,15 @@ class LogicFromWindow
         button('Добавить') { stretchy false}
         button('Изменить') { stretchy false }
         button('Удалить') { stretchy false }
-        button('Обновить') { stretchy false }
+        button('Обновить') { stretchy false
+        on_clicked do
+          puts 123
+        end
+        }
       }
     }
+    on_create
+    root_container
     end
   private
   def sort_by_column(column_index)
