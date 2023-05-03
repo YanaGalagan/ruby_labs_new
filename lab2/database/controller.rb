e8e34c6b7e4ee3cd0435f2fd373a13a6893ee370
Яна Александровна Галаган <s0160132@edu.kubsu.ru>
2023-05-03 21:44:01 +0300

    Revert "что-то страшное произошло..."
    This reverts commit cb571cfb222e6eb327a03c4893ab863265e7c21f.

--- lab2/database/controller.rb
+++ lab2/database/controller.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
+<<<<<<< HEAD
+=======
 
+>>>>>>> origin/master
 require_relative 'window'
 require_relative 'logic'
 require_relative 'student_list'
 require_relative 'student_list_db'
 require_relative '../all_data/data_list_student_short'
+<<<<<<< HEAD
+=======
 require 'glimmer-dsl-libui'
 
+>>>>>>> origin/master
 class StudentListController
   def initialize(view)
     @view = view
@@ -15,17 +21,36 @@ class StudentListController
   end
 
   def on_view_created
+<<<<<<< HEAD
+    @student_list=StudentList.new(StudentListDBAdapter.new)
+=======
     @student_list = StudentList.new(StudentListDBAdapter.new)
+>>>>>>> origin/master
   end
 
   def show_view
     @view.create.show
   end
 
-  def refresh_data(k,n)
-    @data_list = @student_list.k_n_student_short_list(k, n, @data_list)
+<<<<<<< HEAD
+  def refresh_data(k, n)
+    @data_list = @student_list.get_k_n_student_short_list(k, n, @data_list)
     @view.update_student_count(@student_list.count_student)
   end
 end
-
-
+=======
+  def refresh_data(k,n)
+    begin
+    @data_list = @student_list.k_n_student_short_list(k, n, @data_list)
+    @view.update_student_count(@student_list.count_student)
+    rescue
+      on_db_conn_error
+    end
+  end
+  def on_db_conn_error
+    script = 'display dialog "Отсутсвует подключение к БД" with title "Ошибка"'
+    system 'osascript', '-e', script
+    exit(false)
+  end
+end
+>>>>>>> origin/master
