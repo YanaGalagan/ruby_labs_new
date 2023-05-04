1c07e2b9c46d2aba2fcda070095363908859e970
Яна Александровна Галаган <s0160132@edu.kubsu.ru>
2023-05-04 21:37:06 +0300

    добавлено исключение

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@
 source "https://rubygems.org"
 gem "sqlite3"
 gem "glimmer-dsl-libui"
+gem "libui"
 # gem "rails"
--- lab2/database/controller.rb
+++ lab2/database/controller.rb
@@ -4,6 +4,7 @@ require_relative 'logic'
 require_relative 'student_list'
 require_relative 'student_list_db'
 require_relative '../all_data/data_list_student_short'
+require 'glimmer-dsl-libui'
 class StudentListController
   def initialize(view)
     @view = view
@@ -20,7 +21,24 @@ class StudentListController
   end
 
   def refresh_data(k, n)
+    begin
     @data_list = @student_list.get_k_n_student_short_list(k, n, @data_list)
     @view.update_student_count(@student_list.count_student)
+    rescue
+      on_db_conn_error
+      end
+  end
+
+  def on_db_conn_error
+    message_box = MessageBox.new(
+      title: "Ошибка подключения к базе данных",
+      text: "Произошла ошибка при подключении к базе данных. Проверьте настройки подключения и повторите попытку.",
+      parent: @view.get_root,
+      buttons: ['Выйти']
+    )
+    message_box.on_confirm do
+      exit
+    end
+    message_box.show
   end
 end
